5456b3941fc9a7520993d9426c00bed7a2d63fda
Zeyad <zeyad.elsonbaty1@gmail.com>
2025-08-14 19:40:56 +0300

    AddedLenisForSmoothScrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
+import { useEffect, useRef } from "react";
 import AppRoutes from "./routes/AppRoutes";
+import { ReactLenis } from "lenis/react";
+import { cancelFrame, frame } from "framer-motion";
 
 const App = () => {
-  return <AppRoutes />;
+  const lenisRef = useRef(null);
+
+  useEffect(() => {
+    function update(data) {
+      const time = data.timestamp;
+      lenisRef.current?.lenis?.raf(time);
+    }
+    frame.update(update, true);
+
+    return () => cancelFrame(update);
+  }, []);
+  return (
+    <>
+      <ReactLenis root options={{ autoRaf: false }} ref={lenisRef} />
+      <AppRoutes />
+    </>
+  );
 };
 export default App;
